6e75554ba7be8884c479ef39f1ee2e75fef3051b
Ejike <79414070+EjikeOdoh@users.noreply.github.com>
2022-09-30 09:00:15 +0100

    First commit

---
+++ register.js
@@ -0,0 +1,9 @@
+
+
+function handleSubmit(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const formProps = Object.fromEntries(formData);
+
+    console.log(formProps);
+  }
